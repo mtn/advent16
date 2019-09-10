ff402ab86229e513b1c3aacf5ad25fd1ae04ffd7
Michael Noronha <mtn@uber.com>
2019-09-10 14:28:03 -0700

    Day 6

---
+++ day06/part1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict
+
+letter_freqs = [defaultdict(int) for _ in range(len("ciglthza"))]
+
+with open("input.txt") as f:
+    for line in f:
+        word = line.strip()
+
+        for i, c in enumerate(word):
+            letter_freqs[i][c] += 1
+
+print("".join(map(lambda x: max(x, key=x.get), letter_freqs)))
