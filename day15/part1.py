46a00cbac9ba50046031640ff45606ccb1cd2dbc
Michael Noronha <michaeltnoronha@gmail.com>
2020-12-11 22:44:23 -0600

    day 15

---
+++ day15/part1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+st = []
+with open("input.txt") as f:
+    for line in f:
+        splt = line.strip().split()
+        npos = int(splt[3])
+        ipos = int(splt[-1][:-1])
+        st.append((ipos, npos))
+
+t = 0
+while True:
+    valid = True
+    for dt, s in enumerate(st):
+        ipos, npos = s
+        pos = (ipos + t + dt + 1) % npos
+        if pos != 0:
+            valid = False
+            break
+    if valid:
+        print(t)
+        break
+
+    t += 1
