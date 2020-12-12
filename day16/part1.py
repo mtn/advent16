4aa019279661e5ee5233d940adb849b6e63b914c
Michael Noronha <michaeltnoronha@gmail.com>
2020-12-11 22:55:33 -0600

    day 16

---
+++ day16/part1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+inp = "01111010110010011"
+dlen = 272
+
+while len(inp) < dlen:
+    a = inp
+    b = str(a)[::-1].replace("0", "2").replace("1", "0").replace("2", "1")
+    inp = a + "0" + b
+
+inp = inp[:dlen]
+while True:
+    cs = []
+    for i in range(len(inp)//2):
+        if inp[2*i] == inp[2*i+1]:
+            cs.append(1)
+        else:
+            cs.append(0)
+    inp = "".join(map(str, cs))
+
+    if len(inp) % 2 == 1:
+        break
+
+print(inp)
+
